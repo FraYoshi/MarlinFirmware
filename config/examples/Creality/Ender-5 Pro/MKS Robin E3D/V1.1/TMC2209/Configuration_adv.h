73e68d41196aa1c1808a723af707e586d80e3b1f
Francesco Yoshi Gobbo <yoshi@fgobbo.com>
2025-10-24 00:53:42 +0200

    increased bed thermal protection period

--- config/examples/Creality/Ender-5 Pro/MKS Robin E3D/V1.1/TMC2209/Configuration_adv.h
+++ config/examples/Creality/Ender-5 Pro/MKS Robin E3D/V1.1/TMC2209/Configuration_adv.h
@@ -328,7 +328,7 @@
  * Thermal Protection parameters for the bed are just as above for hotends.
  */
 #if ENABLED(THERMAL_PROTECTION_BED)
-  #define THERMAL_PROTECTION_BED_PERIOD        20 // (seconds)
+  #define THERMAL_PROTECTION_BED_PERIOD        60 // (seconds)
   #define THERMAL_PROTECTION_BED_HYSTERESIS     2 // (°C)
 
   /**
