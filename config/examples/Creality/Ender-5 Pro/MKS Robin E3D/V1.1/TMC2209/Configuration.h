1fb0c5afb957ce8a284597de9c367cee4081eda4
Francesco Yoshi Gobbo <yoshi@fgobbo.com>
2025-11-25 06:42:26 +0100

    more permissive bed residency time and hysteresis

--- config/examples/Creality/Ender-5 Pro/MKS Robin E3D/V1.1/TMC2209/Configuration.h
+++ config/examples/Creality/Ender-5 Pro/MKS Robin E3D/V1.1/TMC2209/Configuration.h
@@ -595,9 +595,9 @@
 #endif
 
 #if TEMP_SENSOR_BED
-  #define TEMP_BED_RESIDENCY_TIME     10  // (seconds) Time to wait for bed to "settle" in M190
+  #define TEMP_BED_RESIDENCY_TIME     20  // (seconds) Time to wait for bed to "settle" in M190
   #define TEMP_BED_WINDOW              1  // (°C) Temperature proximity for the "temperature reached" timer
-  #define TEMP_BED_HYSTERESIS          3  // (°C) Temperature proximity considered "close enough" to the target
+  #define TEMP_BED_HYSTERESIS          5  // (°C) Temperature proximity considered "close enough" to the target
 #endif
 
 #if TEMP_SENSOR_CHAMBER
