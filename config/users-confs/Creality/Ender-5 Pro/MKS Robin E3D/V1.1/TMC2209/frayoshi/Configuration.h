bf5ed82b00e18f0e949d97c953fdec8c33712e31
Francesco Yoshi Gobbo <yoshi@fgobbo.com>
2026-09-22 01:22:49 +0200

    lower PLA preheat default

--- config/users-confs/Creality/Ender-5 Pro/MKS Robin E3D/V1.1/TMC2209/frayoshi/Configuration.h
+++ config/users-confs/Creality/Ender-5 Pro/MKS Robin E3D/V1.1/TMC2209/frayoshi/Configuration.h
@@ -2258,7 +2258,7 @@
 //
 #define PREHEAT_1_LABEL       "PLA"
 #define PREHEAT_1_TEMP_HOTEND 140
-#define PREHEAT_1_TEMP_BED     65
+#define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
